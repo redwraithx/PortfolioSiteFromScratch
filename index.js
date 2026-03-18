2226c762a21016524ccf1abe85ec04bceb48d3d0
Brian Purdy <brianpurdy01@gmail.com>
2026-03-18 12:04:06 -0400

    first push of my Portfolio site project

---
+++ index.js
@@ -0,0 +1,155 @@
+
+
+// Project details display logic
+let updatingProjectDisplay = false;
+
+// const projectscontainer = document.getElementById('projects');
+
+const projectLink1 = document.getElementById('project-link-1');
+const project1 = document.getElementById('project1');
+
+const projectLink2 = document.getElementById('project-link-2');
+const project2 = document.getElementById('project2');
+
+const projectLink3 = document.getElementById('project-link-3');
+const project3 = document.getElementById('project3');
+
+// projectscontainer?.addEventListener('mouseleave', (event) => {
+//     displayProject(event, 'hideall', 'none');
+// })
+
+
+// add event listener for each project link, used MouseEnter and MouseLeave events to show/hide project details on hover, instead of click, for better user experience
+projectLink1?.addEventListener('mouseenter', (event) => { {
+    if(!updatingProjectDisplay) {
+        displayProject(event, 'project1', 'block');
+    }
+}});
+// projectLink1?.addEventListener('mouseleave', (event) => {
+//     if(!updatingProjectDisplay) {
+//         displayProject(event, 'project1', 'none');
+//     }
+// });
+
+projectLink2?.addEventListener('mouseenter', (event) => {
+    if(!updatingProjectDisplay) {
+        displayProject(event, 'project2', 'block');
+    }
+});
+// projectLink2?.addEventListener('mouseleave', (event) => {
+//     if(!updatingProjectDisplay) {
+//         displayProject(event, 'project2', 'none');
+//     }
+// });
+
+projectLink3?.addEventListener('mouseenter', (event) => {
+    if(!updatingProjectDisplay) {
+        displayProject(event, 'project3', 'block');
+    }
+});
+// projectLink3?.addEventListener('mouseleave', (event) => {
+//     if(!updatingProjectDisplay) {
+//         displayProject(event, 'project3', 'none');
+//     }
+// });
+
+// initialize project details display state
+project1.style.display = 'block';
+project2.style.display = 'none';
+project3.style.display = 'none';
+
+projectLink1.style.backgroundColor = 'var(--project-select-color)';
+
+projectLink1.style.color = 'var(--menu-active-color)';
+
+
+/**
+ * 
+ * @param {Event} event - the click event that triggered the display of the project details
+ * @param {String} linkName - the name of the project link that was clicked
+ * @param {String} visibility - the visibility state to set for the project details (e.g. 'block' or 'none')
+ */
+function displayProject(event, linkName, visibility) {
+    //console.log("Name is: " + linkName, "Event is: " + event, "event type is: " + event.type);
+
+    if(visibility != 'block' && visibility != 'none') {
+        //console.log("Invalid visibility value: " + visibility);
+        return;
+    }
+
+    if(linkName === 'hideall') {
+        project1.style.display = 'none';
+        project2.style.display = 'none';
+        project3.style.display = 'none';
+        
+        return;
+    }
+
+    if(updatingProjectDisplay) {
+        //console.log("Already updating project display, ignoring click");
+        return;
+    }
+
+    updatingProjectDisplay = true;
+
+    if(linkName === 'project1') {
+        project1.style.display = visibility;
+        project2.style.display = 'none';
+        project3.style.display = 'none';
+
+        projectLink1.style.backgroundColor = 'var(--project-select-color)';
+        projectLink2.style.backgroundColor = 'var(--project-inactive-color)';
+        projectLink3.style.backgroundColor = 'var(--project-inactive-color)';
+
+        projectLink1.style.color = 'var(--menu-active-color)';
+        projectLink2.style.color = 'var(--menu-base-color)';
+        projectLink3.style.color = 'var(--menu-base-color)';
+    } 
+    else if(linkName === 'project2') {
+        project1.style.display = 'none';
+        project2.style.display = visibility;
+        project3.style.display = 'none';
+
+        projectLink1.style.backgroundColor = 'var(--project-inactive-color)';
+        projectLink2.style.backgroundColor = 'var(--project-select-color)';
+        projectLink3.style.backgroundColor = 'var(--project-inactive-color)';
+
+        projectLink1.style.color = 'var(--menu-base-color)';
+        projectLink2.style.color = 'var(--menu-active-color)';
+        projectLink3.style.color = 'var(--menu-base-color)';
+    } 
+    else if(linkName === 'project3') {
+        project1.style.display = 'none';
+        project2.style.display = 'none';
+        project3.style.display = visibility;
+
+        projectLink1.style.backgroundColor = 'var(--project-inactive-color)';
+        projectLink2.style.backgroundColor = 'var(--project-inactive-color)';
+        projectLink3.style.backgroundColor = 'var(--project-select-color)';
+
+        projectLink1.style.color = 'var(--menu-base-color)';
+        projectLink2.style.color = 'var(--menu-base-color)';
+        projectLink3.style.color = 'var(--menu-active-color)';
+    }
+    else {
+        console.log("Invalid link name: " + linkName);
+    }
+
+
+
+    updatingProjectDisplay = false;
+}
+
+
+
+
+
+// Skills section display logic
+
+
+
+
+
+
+
+
